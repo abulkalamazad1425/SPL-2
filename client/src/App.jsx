6beb41c91aeadc05829073ae27a060699be66508
abulkalamazad1425 <bsse1425@iit.du.ac.bd>
2025-03-05 17:35:25 +0600

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import RegVerification from './pages/RegVerification';
@@ -11,38 +11,77 @@ import PaymentFailed from './pages/PaymentFailed';
 import PaymentCancelled from './pages/PaymentCancelled';
 import Profile from './pages/Profile';
 import PrivateRoute from './components/PrivateRoute';
+import PrivateRouteStudent from './components/PrivateRoute';
 import UpdataMeal from './pages/UpdataMeal';
 import AddManager from './pages/AddManager';
 import PrivateRouteTeacher from './components/PrivateRouteTeacher';
 import PrivateRouteManager from './components/PrivateRouteManager';
 import MealSchedule from './pages/MealSchedule';
 import Payment from './pages/Payment';
+import UploadNotice from './pages/UploadNotice';
+import GiveFeedback from './pages/GiveFeedback';
+import UploadExpense from './pages/UploadExpense';
+import SurveyTool from './pages/SurveyTool';
+import SurveyResponse from './pages/SurveyResponse';
+import Notice from './pages/Notice';
+import NoticeDetails from './pages/NoticeDetails';
+import Expense from './pages/Expense';
+import ExpenseDetails from './pages/ExpenseDetails';
+import SurveyList from './pages/SurveyList';
+import SurveyResults from './pages/SurveyResults';
+import FeedbackList from './pages/FeedbackList';
+import MealList from './pages/MealList';
+import RefundList from './pages/RefundList';
+import PaymentHistory from './pages/PaymentHistory';
+import MealHistory from './pages/MealHistory';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/registration_verification" element={<RegVerification/>}/>
-        <Route path="/registration" element={<Registration/>}/>
-        <Route path="/login" element={<LogIn/>}/>
-        <Route path='/forget_password' element={<ForgetPass/>}/>
-        <Route path='/payment_success/:tranId' element={<PaymentSuccess/>}/> 
-        <Route path='/payment_failed' element={<PaymentFailed/>}/> 
-        <Route path='/payment_cancelled' element={<PaymentCancelled/>}/>
-        <Route element={<PrivateRoute/>}>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/update_mealstatus' element={<UpdataMeal/>}/>
-          <Route path='/payment' element={<Payment/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/registration_verification" element={<RegVerification />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/forget_password" element={<ForgetPass />} />
+        <Route path="/payment_success/:tranId" element={<PaymentSuccess />} />
+        <Route path="/payment_failed" element={<PaymentFailed />} />
+        <Route path="/payment_cancelled" element={<PaymentCancelled />} />
+        <Route path="/view_notice" element={<Notice />} />
+        <Route path="/view_notice_details/:noticeId" element={<NoticeDetails />} />
+        <Route path="/view_expense" element={<Expense />} />
+        <Route path="/view_expense_details/:expenseId" element={<ExpenseDetails />} />
+        <Route path="/view_survey" element={<SurveyList />} />
+        <Route element={<PrivateRouteStudent />}>
+          <Route path="/update_mealstatus" element={<UpdataMeal />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/give_feedback" element={<GiveFeedback />} />
+          <Route path="/payment_history/:studentId" element={<PaymentHistory />} />
+          <Route path="/meal_history/:studentId" element={<MealHistory />} />
+          <Route path="/survey_response/:surveyId" element={<SurveyResponse />} />
         </Route>
-        <Route element={<PrivateRouteTeacher/>}>
-          <Route path='add_manager' element={<AddManager/>}/>
+        <Route element={<PrivateRouteTeacher />}>
+          <Route path="/add_manager" element={<AddManager />} />
         </Route>
-        <Route element={<PrivateRouteManager/>}>
-          <Route path='update_mealschedule' element={<MealSchedule/>}/>
+        <Route element={<PrivateRouteManager />}>
+          <Route path="/update_mealschedule" element={<MealSchedule />} />
+          <Route path="/upload_notice" element={<UploadNotice />} />
+          <Route path="/upload_expense" element={<UploadExpense />} />
+          <Route path="/create_survey" element={<SurveyTool />} />
+          <Route path="/student_feedback" element={<FeedbackList />} />
+          <Route path="/view_meal_list" element={<MealList />} />
+          <Route path="/view_survey_result/:surveyId" element={<SurveyResults />} />
+          <Route path="/view_refund_list" element={<RefundList />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
